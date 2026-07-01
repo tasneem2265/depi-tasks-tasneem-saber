b5d6ae75629e0145552bf4296639fb10ecf4e22c
tasneem2265 <tasneem2005saber@gmail.com>
2026-07-02 01:47:43 +0300

    Update BookingsController.cs

--- task5/Areas/Admin/Controllers/BookingsController.cs
+++ task5/Areas/Admin/Controllers/BookingsController.cs
@@ -1,1 +1,31 @@
-.
+using CinemaBooking.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class BookingsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var bookings = await _context.Bookings
+                .Include(b => b.User)
+                .Include(b => b.Showtime).ThenInclude(s => s.Movie)
+                .Include(b => b.Showtime).ThenInclude(s => s.Hall).ThenInclude(h => h.Cinema)
+                .OrderByDescending(b => b.BookingDate)
+                .ToListAsync();
+
+            return View(bookings);
+        }
+    }
+}
